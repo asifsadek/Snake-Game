a0922f48f67520c074e99dce828def452509cc13
Omar Shafik <o.shafik13@gmail.com>
2018-09-13 00:01:57 +0200

    Update pixel.h

--- pixel.h
+++ pixel.h
@@ -10,13 +10,9 @@ class Pixel : public QWidget
 public:
 
     Pixel(int x,int y);
-    void set_pos(int x , int y);
-    void set_colour_black();
-    void set_colour_white();
-    void set_colour_red();
-    int get_x();
-    int get_y();
     Pixel* next;
+    int x;
+    int y;
 };
 
 #endif // PIXEL_H
