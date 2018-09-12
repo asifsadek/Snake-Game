50b7a74c410846b27dddf547f9a74b8e6cfba2c5
Omar Shafik <o.shafik13@gmail.com>
2018-09-12 23:39:57 +0200

    Update snake.h

--- snake.h
+++ snake.h
@@ -3,17 +3,29 @@
 
 #include <QObject>
 #include "pixel.h"
+#include <QPainter>
+#include <QPaintEvent>
+#include <QRect>
+#include <QBrush>
 
 class Snake
 {
+private:
+    enum Direction {
+      Up,Right,Down,Left
+    };
 
 public:
-
+    Direction direction;
     Pixel* head;
     Pixel* tail;
     int length;
-    Snake();
+    Snake(QPainter* painter);
+    void move();
+    void draw();
+    QPainter* s_painter;
     void keyPressEvent(QKeyEvent* event);
+
 
 };
 
