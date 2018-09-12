e87108c2f43ac9ca46ba9d0f606b0c9400e06f69
Omar Shafik <o.shafik13@gmail.com>
2018-09-12 23:39:20 +0200

    Update snake.cpp

--- snake.cpp
+++ snake.cpp
@@ -1,15 +1,50 @@
 #include "snake.h"
 #include <QKeyEvent>
 
-Snake::Snake()
+#define BLACK_BRUSH *new QBrush(*new QColor(0,0,0))
+#define WHITE_BRUSH *new QBrush(*new QColor(255,255,255))
+
+Snake::Snake(QPainter* painter)
 {
-    head=new Pixel(10,10);
-    tail=new Pixel(20,10);
+    head=new Pixel(10,20);
+    tail=new Pixel(20,20);
     tail->next=head;
     length = 2;
     direction=Right;
+    s_painter=new QPainter();
+    s_painter=painter;
+    draw();
 
 }
+
+void Snake::move() {
+    Pixel* temp;
+    temp = this->tail;
+    this->tail = temp->next;
+    //assign new head position
+    switch(direction) {
+    case Up:
+        this->head->set_pos(head->get_x(),head->get_y()-10);
+        break;
+    case Right:
+        this->head->set_pos(head->get_x()+10,head->get_y());
+        break;
+    case Down:
+        this->head->set_pos(head->get_x(),head->get_y()+10);
+        break;
+    case Left:
+        this->head->set_pos(head->get_x()-10,head->get_y());
+        break;
+    }
+    draw();
+}
+
+void Snake::draw() {
+    s_painter->fillRect(0,0,700,700,WHITE_BRUSH);
+    s_painter->fillRect(head->get_x(),head->get_y(),10,10,BLACK_BRUSH);
+    s_painter->fillRect(tail->get_x(),tail->get_y(),10,10,BLACK_BRUSH);
+}
+
 
 void Snake::keyPressEvent(QKeyEvent *event){
     Pixel* temp;
