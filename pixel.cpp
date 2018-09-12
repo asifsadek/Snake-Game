5d2d8fca87f2a06022940029ecb208508ff5c0aa
Omar Shafik <o.shafik13@gmail.com>
2018-09-13 00:02:33 +0200

    Update pixel.cpp

--- pixel.cpp
+++ pixel.cpp
@@ -1,41 +1,11 @@
 #include "pixel.h"
 Pixel::Pixel(int x,int y)
 {
-    this->setGeometry(x, y, 10, 10);
-    this->setStyleSheet("background-color:white;");
-    this->show();
+    this->x = x;
+    this->y = y;
 }
 
-void Pixel::set_pos(int x , int y)
-{
-    this->setGeometry(x, y, 10, 10);
-    this->show();
-}
-void Pixel::set_colour_black()
-{
-    this->setStyleSheet("background-color:black;");
-    this->show();
-}
-void Pixel::set_colour_white()
-{
-    this->setStyleSheet("background-color:white;");
-    this->show();
-}
-void Pixel::set_colour_red()
-{
-    this->setStyleSheet("background-color:red;");
-    this->show();
-}
 
-int Pixel:: get_x()
-{
-    return this->x();
-}
-
-int Pixel:: get_y()
-{
-    return this->y();
-}
 
 
 
