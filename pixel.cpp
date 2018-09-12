e758b25d96359434429735a781bc1df2ff61a574
Abdallah-Elshamy <33662044+Abdallah-Elshamy@users.noreply.github.com>
2018-09-12 21:35:52 +0200

    Update pixel.cpp

--- pixel.cpp
+++ pixel.cpp
@@ -1,6 +1,36 @@
 #include "pixel.h"
-
-Pixel::Pixel()
+Pixel::Pixel(int x,int y)
 {
-
+    this->setGeometry(x, y, 10, 10);
+    this->setStyleSheet("background-color:white;");
+    this->show();
 }
+
+void Pixel::set_pos(int x , int y)
+{
+    this->setGeometry(x, y, 10, 10);
+    this->show();
+}
+void Pixel::set_colour_black()
+{
+    this->setStyleSheet("background-color:black;");
+    this->show();
+}
+void Pixel::set_colour_red()
+{
+    this->setStyleSheet("background-color:red;");
+    this->show();
+}
+
+int Pixel:: get_x()
+{
+    return this->x();
+}
+
+int Pixel:: get_y()
+{
+    return this->y();
+}
+
+
+
